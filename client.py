a90613eb0df3deb0f0c40fe2a447a6cfab004702
rowlanja <rowlanja@tcd.ie>
2021-04-21 21:44:19 +0100

    finished

--- client.py
+++ client.py
@@ -18,15 +18,15 @@ def verify():
     user = "Paulo"
 
 #sending will send a file to the server
-def sending(username):
-    filename = "sample.txt"  # File wanting to send
+def sending(username, upload_filename):
+    filename = "client_files/"+upload_filename  # File wanting to send
     f = open(filename, 'rb')  # Open file
     buf = 4000  # Buffer size
 
     while (True):
         l = f.read(buf) #read buffer-sized byte section of the file
         if len(l) < 1: closing() #if there is no more of the file to be read, close it and end program
-        l = str.encode("__verify__msg"+username+",") + l
+        l = str.encode("__verify__msg"+username+","+upload_filename+",") + l
         cipher = AES.new(key, AES.MODE_EAX) #create cipher object for encryption
         nonce = cipher.nonce #generate nonce number
         ciphertext, tag = cipher.encrypt_and_digest(l) #encrypt f and generate a tag for integrity-checking
@@ -38,13 +38,13 @@ def sending(username):
 
 
 #receiving will recieve a file from the server
-def recieving():
+def recieving(username, filename):
     buf = 4096 #reading buffer size
-    filename = b"files/sample.txt"  # File wanting to recieve
-    fnew = open(filename.decode('utf-8'), 'wb') #file name for new file
+    original_file = filename    #keep copy of filename sent to server
+    filename = filename.encode()  # File wanting to recieve
 
     # concatinate isafile with requested filename so it can be distingueshed as a client-recieving command
-    filename = b'isafile' + filename
+    filename = username.encode() + b'isafile' + filename
     #print("sending {}".format(filename))
     s.sendto(filename, server_address) #send requested filename to server
 
@@ -56,7 +56,7 @@ def recieving():
     r.bind(new_server_address) #bind the socket to the address
 
     color_print("\n[!] waiting for a connection ", color="red", underline=True)
-
+    complete_plaintext = ''
     while (True):
         #if failed, will throw socket.timeout exception and file/socket will be closed/exited
         try:
@@ -80,33 +80,47 @@ def recieving():
                             label=None
                         )
                     )
-                    print("decrypted with keys : ", original_message)
+                    #print("decrypted with keys : ", original_message)
                     original_message, ignore, nonce = original_message.rpartition(b'uniqueword') #separate nonce from ciphertext variable
                     original_message, ignore, tag = original_message.rpartition(b'uniqueword')   #separate ciphertext and tag from ciphertext variable
-                    print("nonce : ", nonce, " tag : ", tag)
+                    #print("nonce : ", nonce, " tag : ", tag)
                     ciphertext, address = r.recvfrom(buf) #begin recieving file
                     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce) #create cipher object for decryption
                     plaintext = cipher.decrypt(ciphertext) #decrypt cipher text
-                    print("recieved AES msg : ", plaintext)
                     #try to verify message with tag. If its been changed in transit, throw ValueError and close file/socket and exit
                     try:
                         cipher.verify(tag) #verify the tag to check integrity
                         print("The download is authentic")
+                        complete_plaintext += plaintext.decode()
                     except ValueError:
                         print("Key incorrect or message corrupted")
                         print('closing')
-                        fnew.close()
                         s.close()
                         exit()
-                    fnew.write(plaintext) #write data to the new file
 
         except socket.timeout:
-            print('closing')
-            fnew.close()
+            # if we recieved the file list we print it out to the user and ask for what file from the file list they want to recieve
+            if original_file == "server_files/file_list.txt" :
+                print('finished recieving file list : ', complete_plaintext)
+                return complete_plaintext
+            else :
+            # else we have received a file and just save it to the user files
+                print('finished recieving file : ', complete_plaintext)
+                fnew = open('client_files/download.txt', 'wb') #file name for new file
+                fnew.write(complete_plaintext.encode()) #write data to the new file
+                fnew.close()
+
             s.close()
             r.close()
             exit()
     exit()
+
+def pick_file(file_list):
+    file_list = file_list.split()
+    for index in range(len(file_list)):
+        print("[%d] %s" % (index, file_list[index]))
+    req_index = input("[!] Enter index of requested file : ")
+    return file_list[int(req_index)]
 # #create our public/private keys for the client
 # private_key = rsa.generate_private_key(
 #     public_exponent=65537,
@@ -139,14 +153,20 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = (socket.gethostname(), 10000)
 # Generate key for AES encryption
 key = b'Sixteen byte key'
-username = input("Please enter your username : ")
-cors = input("Are you receiving or sending? (r or s)")
-
+# username = input("Please enter your username : ")
+# cors = input("Are you receiving or sending? (r or s)")
+username = 'Joeseph'
+cors = 'r'
 #if sending a file, go to sending function, else if receiving a file go to receiving function, else repeat
 while True:
     if cors == 'r' or cors == 'R':
-        recieving()
+        file_list = recieving(username, "server_files/file_list.txt")
+        file_choice = pick_file(file_list)
+        print("requesting ", file_choice)
+        file_list = recieving(username, ("server_files/files/"+file_choice))
     elif cors == 's' or cors == 'S':
-        sending(username)
+        # upload_filename = input("Upload : ")
+        upload_filename = 'sample_rmv_user.txt'
+        sending(username, upload_filename)
     else:
         cors = input("Enter r or s (r or s)")
--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-#socket_echo_server.py
 import socket
 from Cryptodome.Cipher import AES
 import time
-import os.path
+import os.path, os
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
@@ -10,15 +9,14 @@ from cryptography.hazmat.primitives import serialization
 from lazyme.string import color_print
 
 # closing will close the file and exit the client program
-def closing():
-    print('closing socket')
-    s.close()
-    exit()
+# def closing():
+#     print('closing socket')
+#     s.close()
+#     exit()
 
 #sending will send a file to the client
-def sending(fname):
+def sending(name, fname):
     server_address = (socket.gethostname(), 10100) #create a new socket address
-    print("sending")
     # #if the file doesnt exit, close the socket and exit program
     # if not (os.path.exists(fname.decode())):
     #     print("file not found, closing")
@@ -26,14 +24,17 @@ def sending(fname):
     #     exit()
 
     #f = open(fname.decode('utf-8'), 'rb') #open the requested file
-    message = open('sample.txt', 'r') #open the requested file
+    print("sending : ", fname)
+    message = open(fname.decode(), 'r') #open the requested file
     buffer = 4000  # Buffer size
     key = b'Sixteen byte key' # key = get_random_bytes(16)
 
     while (True):
         snippet = message.read(buffer) #read buffer-sized byte section of the file
-        if len(snippet) < 1: closing() #if there is no more of the file to be read, close it and end program
-        with open("clientKeys/public_key.pem", "rb") as key_file:
+        user_pem = 'server_files/keys/'+name.decode()+'.pem'
+        print('using pem file to encrypt : ', user_pem)
+        if len(snippet) < 1: break#if there is no more of the file to be read, close it and end program
+        with open(user_pem, "rb") as key_file:
             public_key = serialization.load_pem_public_key(
                 key_file.read(),
                 backend=default_backend()
@@ -61,39 +62,49 @@ def sending(fname):
 
 #receiving will recieve a file from the client
 def receiving(ciphertext):
-    f = open('files/sample.txt', 'wb') #open file that will be written to
     try:
         while (True):
             ciphertext, ignore, nonce = ciphertext.rpartition(b'uniqueword') #separate nonce from ciphertext variable
             ciphertext, ignore, tag = ciphertext.rpartition(b'uniqueword')   #separate ciphertext and tag from ciphertext variable
 
-            print('received {}'.format(ciphertext))
+            #print('received {}'.format(ciphertext))
             cipher = AES.new(key, AES.MODE_EAX, nonce=nonce) #create cipher object for decryption
             plaintext = cipher.decrypt(ciphertext)           #decrypt cipher text
             decoded_plaintext = plaintext.decode()
-            instruction = decoded_plaintext[0:13:1]
+            instruction = decoded_plaintext[0:13:1]     # find out what instruction
             decoded_plaintext = decoded_plaintext[13:]  # discard extracted information
+            print('instruction : ', instruction)
             if instruction == "__verify__msg":
-                username = decoded_plaintext.split(",")[0]
-                verified = verification(username)
-            elif instruction == "__verify__add":
-                print("nothing")
-            elif instruction == "__verify__rmv":
-                print("nothing")
-            decoded_plaintext = decoded_plaintext.replace(username+",", "") #removed name
+                username = decoded_plaintext[:decoded_plaintext.find(",")]  # find username appended to front of original_mesage
+                decoded_plaintext = decoded_plaintext.replace(username+',', '')                 # discard username
+                filename = decoded_plaintext[:decoded_plaintext.find(",")]  # find filename appended to front of original_message
+                decoded_plaintext = decoded_plaintext.replace(filename+',', '')                 # discard filename
+                print("username : ", username, " filename : ", filename)
+                verified = verification(username.encode())
+            if decoded_plaintext[0:13:1] == "__verify__add":
+                decoded_plaintext = decoded_plaintext[13:]+" "  # find username appended to front of original_mesage
+                print("adding : ", decoded_plaintext)
+                add_user(decoded_plaintext)
+                verified = verification(username.encode())
+            elif decoded_plaintext[0:13:1] == "__verify__rmv":
+                decoded_plaintext = decoded_plaintext[13:]+" "  # find username appended to front of original_mesage
+                print("removing : ", decoded_plaintext)
+                rmv_user(decoded_plaintext)
+                verified = verification(username.encode())
             # try to verify message with tag. If its been changed in transit, throw ValueError and close file/socket and exit
             try:
                 if not verified :
                     raise ValueError
                 cipher.verify(tag) #verify the tag to check integrity
                 print("The message is authentic : ", decoded_plaintext, " from : ", username)
+                f = open('server_files/files/'+filename, 'wb') #open file that will be written to
+                f.write(decoded_plaintext.encode())
             except ValueError:
                 print("Key incorrect or message corrupted or access from unverified user")
                 print('Closing')
-                f.close()
                 s.close()
                 exit()
-            f.write(plaintext)
+
             s.settimeout(2)
             ciphertext, address = s.recvfrom(buf)
 
@@ -104,15 +115,56 @@ def receiving(ciphertext):
         exit()
 
 def verification(username):
+    username = username.decode().strip()
     f = open("server_files/banned_users.txt")
     for x in f:
         if username == x :
             msg = "Message from bad user : " + username + "\n"
             color_print(msg, color="red", underline=True)
             return 0
-    return 1
+    f = open("server_files/verified_users.txt")
+    for x in f:
+        if username == x.strip() :
+            msg = "Message from verified : " + username + "\n"
+            color_print(msg, color="green", underline=True)
+            return 1
+    msg = "Message from bad user : " + username + "\n"
+    color_print(msg, color="red", underline=True)
+    return 0
 
-# def add_user(username):
+def add_user(new_user_info):
+    key_start = new_user_info.find('__verify__key') + 13
+    username = new_user_info[:(new_user_info.find('__verify__key')-1)].strip()
+    print('un : ', username)
+    key = new_user_info[key_start:]
+    new_user_pem_file = 'server_files/keys/' + username + '.pem'
+    new_user_pem_file = new_user_pem_file.strip()
+    write_verified = open("server_files/verified_users.txt", 'a')
+    write_key = open(new_user_pem_file, 'wb')
+    write_verified.write(username + "\n")
+    write_key.write(key.encode())
+
+def rmv_user(new_user_info):
+    username = new_user_info[:(new_user_info.find('__verify__key')-1)].strip()
+    verified_file = "server_files/verified_users.txt"
+    print('un : ', username)
+    if os.path.exists("server_files/keys/"+username+".pem"):
+        os.remove("server_files/keys/"+username+".pem")
+        filename = "server_files/verified_users.txt"
+    else:
+      print("The user is not a verified member in the group")
+      return
+    read = open(verified_file)
+    read = list(read)
+    open(verified_file, 'w').close() # clears the file
+    print("read : ", read)
+    with open(verified_file, 'w') as f:
+      for item in read:
+          print(item.strip(), " vs ",username )
+          if item.strip() != username :
+              print("writing : ", item)
+              f.write("%s" % item)
+    print("[!] User removed ", username)
 
 
 # Create a UDP/IP socket
@@ -128,17 +180,23 @@ key = b'Sixteen byte key' # Generate key for AES encryption
 
 
 print('waiting for a connection')
-ciphertext, address = s.recvfrom(buf) #recieve ciphertext sent
+while(True):
+    ciphertext, address = s.recvfrom(buf) #recieve ciphertext sent
+    name_index = ciphertext.find(b'isafile')
+    name = ciphertext[:name_index]
+    print('recieved : ',ciphertext)
+    #if there is an isafile in a message, call sending function, else call receiving function
+    ignore1, ignore2, filename = ciphertext.rpartition(b'isafile')
+    print("the bitch says : ",
+        "name : ", name,
+        "ignore1 : ", ignore1,
+        "ignore2 : ", ignore2,
+        "filename : ", filename,
+    )
+    if verification(name):
+        if ignore2:
 
-#if there is an isafile in a message, call sending function, else call receiving function
-ignore1, ignore2, filename = ciphertext.rpartition(b'isafile')
-# print("the bitch says : ",
-#     "ctext : ", ciphertext,
-#     "ignore1 : ", ignore1,
-#     "ignore2 : ", ignore2,
-#     "filename : ", filename,
-# )
-if ignore2:
-    sending(filename)
-else:
-    receiving(ciphertext)
+                print("filename : ", filename)
+                sending(name, filename)
+        else:
+            receiving(ciphertext)
